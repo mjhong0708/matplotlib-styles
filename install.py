58134dcaf421c61e87a867939dfcb17189c693b6
Minjoon Hong <mjhong0708@yonsei.ac.kr>
2022-08-15 15:08:18 +0900

    Fixed install script

--- install.py
+++ install.py
@@ -1,30 +1,31 @@
 #!/usr/bin/env python
-import subprocess
 from pathlib import Path
 
 
 def create_symlinks(src_dir, target_dir):
-    if not target_dir.isdir():
+    if not target_dir.is_dir():
         target_dir.mkdir()
+        print(f"Created directory {target_dir}")
 
     all_files = list(Path(src_dir).glob("*"))
     for file in all_files:
         target = target_dir / file.name
         if not target.is_symlink():
-            target.symlink_to(file)
+            src_abs = file.absolute()
+            target.symlink_to(src_abs)
             print(f"Created symlink for {file}")
 
 def remove_broken_symlinks(target_dir):
     for f in target_dir.glob("*"):
         if f.is_symlink() and not f.exists():
             f.unlink()
-            print(f"Removed symlink for {file}")
+            print(f"Removed symlink for {f}")
 
 
 if __name__ == "__main__":
     src_dir = Path("stylesheets")
     target_dir = Path("../stylelib")
 
-    create_symlinks(src_dir)
+    create_symlinks(src_dir, target_dir)
     remove_broken_symlinks(target_dir)
 
